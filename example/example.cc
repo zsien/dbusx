93a37b135692a0713c36107770f5b3e7a99e325e
zsien <i@zsien.cn>
2022-05-27 21:14:51 +0800

    chore: update examples

--- example/example.cc
+++ example/example.cc
@@ -11,25 +11,17 @@ uint32_t example::string_length(const std::string &str) {
     return str.length();
 }
 
-tl::expected<uint32_t, dbusx::error> example::string_length_e(const std::string &str) {
-    return str.length();
-}
+tl::expected<std::string, dbusx::error> example::may_error(bool should) {
+    if (should) {
+        return tl::make_unexpected(
+            dbusx::error("org.freedesktop.DBus.Error.FileNotFound", "error message"));
+    }
 
-int32_t example::always_error(int32_t) {
-    throw dbusx::error("org.freedesktop.DBus.Error.FileNotFound", "error message");
-}
-
-tl::expected<int32_t, dbusx::error> example::always_error_e(int32_t) {
-    return tl::make_unexpected(
-        dbusx::error("org.freedesktop.DBus.Error.FileNotFound", "error message"));
+    return std::string("no error~");
 }
 
 std::string example::echo(const std::string &str) {
     return str;
-}
-
-std::string example::interface_name() {
-    return "cn.zsien.dbusx.example.control";
 }
 
 std::string example::get_read_only_propery() {
@@ -49,23 +41,25 @@ void example::set_writable_propery(const std::string &str) {
     writable_property_ = str;
 }
 
+std::string example::interface_name() {
+    return "cn.zsien.dbusx.example.control";
+}
+
 dbusx::vtable::vtable example::exported() {
     return {
         .methods =
             {
-                {"stringLength", dbusx::method<&example::string_length>::get_vtable()},
-                {"stringLengthE", dbusx::method<&example::string_length_e>::get_vtable()},
-                {"alwaysError", dbusx::method<&example::always_error>::get_vtable()},
-                {"alwaysErrorE", dbusx::method<&example::always_error_e>::get_vtable()},
-                {"echo", dbusx::method<&example::echo>::get_vtable()},
+                {"StringLength", dbusx::method<&example::string_length>::get_vtable()},
+                {"MayError", dbusx::method<&example::may_error>::get_vtable()},
+                {"Echo", dbusx::method<&example::echo>::get_vtable()},
             },
         .properties =
             {
-                {"readOnlyProperty",
+                {"ReadOnlyProperty",
                  dbusx::property<&example::get_read_only_propery>::get_vtable()},
-                {"readOnlyPropertyE",
+                {"ReadOnlyPropertyE",
                  dbusx::property<&example::get_read_only_propery_e>::get_vtable()},
-                {"writableProperty",
+                {"WritableProperty",
                  dbusx::property<&example::get_writable_propery,
                                  &example::set_writable_propery>::get_vtable()},
             },
--- example/example.h
+++ example/example.h
@@ -11,11 +11,9 @@ public:
     example();
 
     uint32_t string_length(const std::string &str);
-    int32_t always_error(int32_t);
     std::string echo(const std::string &str);
 
-    tl::expected<uint32_t, dbusx::error> string_length_e(const std::string &str);
-    tl::expected<int32_t, dbusx::error> always_error_e(int32_t);
+    tl::expected<std::string, dbusx::error> may_error(bool should);
 
     std::string get_read_only_propery();
     tl::expected<std::string, dbusx::error> get_read_only_propery_e();
